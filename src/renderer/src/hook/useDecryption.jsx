bcd3a36cf01360a11301445f92e4ac96dc292fc6
praisespjmt <praisesmusa@outlook.com>
2024-06-08 22:39:32 +0100

    `feat:` add useDecrypt custom hook

---
+++ src/renderer/src/hook/useDecryption.jsx
@@ -0,0 +1,57 @@
+import { useState } from 'react'
+
+/**
+ * Custom hook for managing the decryption process.
+ *
+ * @param {Array} steps - An array of steps for the decryption process.
+ * @return {Object} - An object containing the current step, boolean flags for the first and last steps,
+ * the current step's content, the array of steps, and functions for navigating to the next or previous step,
+ * and resetting the process.
+ */
+const useDecryption = (steps) => {
+  const [currentStep, setCurrentStep] = useState(0)
+
+  /**
+   * Increments the current step by 1 if it is less than the length of the steps array.
+   *
+   * @return {void}
+   */
+  const next = () => {
+    setCurrentStep((prev) => {
+      if (prev >= steps.length) return prev
+      return prev + 1
+    })
+  }
+
+  /**
+   * Decrements the current step by 1 if it is greater than 0.
+   *
+   * @return {void}
+   */
+  const back = () => {
+    setCurrentStep((prev) => {
+      if (prev <= 0) return prev
+      return prev - 1
+    })
+  }
+
+  /**
+   * Resets the current step to 0.
+   *
+   * @return {void} This function does not return a value.
+   */
+  const reset = () => setCurrentStep(0)
+
+  return {
+    currentStep,
+    isFirstStep: currentStep === 0,
+    isLastStep: currentStep === steps.length - 1,
+    step: steps[currentStep],
+    steps,
+    next,
+    back,
+    reset
+  }
+}
+
+export default useDecryption
