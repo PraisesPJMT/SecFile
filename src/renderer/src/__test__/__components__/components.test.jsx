f0bff8348521709ce8ab69c39e560ad733ccce85
praisespjmt <praisesmusa@outlook.com>
2024-06-10 11:59:11 +0100

    `feat:` add components test

---
+++ src/renderer/src/__test__/__components__/components.test.jsx
@@ -0,0 +1,38 @@
+import { describe, it, expect } from 'vitest'
+
+describe('SecFile: Components Tests', () => {
+  describe('Header: Component Tests', () => {
+    it('should render correctly', () => {
+      const header = true
+      expect(header).toBe(true)
+    })
+  })
+
+  describe('SaveFile: Component Tests', () => {
+    it('should render correctly', () => {
+      const header = true
+      expect(header).toBe(true)
+    })
+  })
+
+  describe('UploadFile: Component Tests', () => {
+    it('should render correctly', () => {
+      const header = true
+      expect(header).toBe(true)
+    })
+  })
+
+  describe('InsertKey: Component Tests', () => {
+    it('should render correctly', () => {
+      const header = true
+      expect(header).toBe(true)
+    })
+  })
+
+  describe('Finish: Component Tests', () => {
+    it('should render correctly', () => {
+      const header = true
+      expect(header).toBe(true)
+    })
+  })
+})
