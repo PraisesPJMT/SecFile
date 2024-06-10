9b69d51c0cab2185d98bed9183b3f862b9a68120
praisespjmt <praisesmusa@outlook.com>
2024-06-10 11:59:34 +0100

    `feat:` add screen tests

---
+++ src/renderer/src/__test__/__screen__/screen.test.jsx
@@ -0,0 +1,31 @@
+import { describe, it, expect } from 'vitest'
+
+describe('SecFile: Screen Tests', () => {
+  describe('Home Screen: Component Tests', () => {
+    it('should render correctly', () => {
+      const header = true
+      expect(header).toBe(true)
+    })
+  })
+
+  describe('Encryption Screen: Component Tests', () => {
+    it('should render correctly', () => {
+      const header = true
+      expect(header).toBe(true)
+    })
+  })
+
+  describe('Decryption: Component Tests', () => {
+    it('should render correctly', () => {
+      const header = true
+      expect(header).toBe(true)
+    })
+  })
+
+  describe('About Software Screen: Component Tests', () => {
+    it('should render correctly', () => {
+      const header = true
+      expect(header).toBe(true)
+    })
+  })
+})
